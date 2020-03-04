60c7d25ffdf4350096085d03f1323af2bfdf2ec4
Brittany Russell <sincerelybr@gmail.com>
2020-03-04 00:23:53 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,20 @@
+# def unsafe?(speed)
+#   if speed < 40 
+#     return true
+#   elsif speed > 60
+#     return true 
+#   else
+#     return false
+#   end
+# end
+
 def unsafe?(speed)
-  if speed < 40 
+  if speed < 40 || speed > 60
     return true
-  elsif speed > 60
-    return true 
   else
     return false
   end
 end
-
 
 
 def not_safe?(speed)
